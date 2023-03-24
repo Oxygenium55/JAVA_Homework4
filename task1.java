e5fe584897f71902efbc47e7bdb2b158a778de36
Oxygen <grandnorik@mail.ru>
2023-03-24 17:03:33 +0600

    1 and 2

--- task1.java
+++ task1.java
@@ -1,7 +1,25 @@
+import java.util.LinkedList;
+
 //Пусть дан LinkedList с несколькими элементами. 
 //Реализуйте метод(не void), который вернет “перевернутый” список.
 
 public class task1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> link_list = new LinkedList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            link_list.add(getRandomNumber());
+        }
+        System.out.println(link_list);
 
-    
+        for (int i = 0; i < 10; i++) {
+            System.out.print(link_list.getLast() + ", ");
+            link_list.removeLast();
+        }
+    }
+
+    public static int getRandomNumber() {
+        double x = (Math.random() * 99);
+        int num = (int) x;
+        return num;
+    }
 }
--- task2.java
+++ task2.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 /* Реализуйте очередь с помощью LinkedList со следующими методами:
 enqueue() - помещает элемент в конец очереди,
 dequeue() - возвращает первый элемент из очереди и удаляет его,
@@ -5,5 +7,39 @@ first() - возвращает первый элемент из очереди, 
 */
 
 public class task2 {
+    public static void main(String[] args) {
+        LinkedList<Integer> link_list = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            enqueue(link_list, getRandomNumber());
+        }
+        System.out.println(link_list);
+
+        System.out.println(dequeue(link_list));
+        System.out.println(link_list);
+
+        System.out.println(first(link_list));
+        System.out.println(link_list); 
+    }
+
+    public static int getRandomNumber() {
+        double x = (Math.random() * 99);
+        int num = (int) x;
+        return num;
+    }
+    public static void enqueue(LinkedList<Integer> ll, int num) {
+        ll.addLast(num);
+    }
+
+    public static int dequeue(LinkedList<Integer> ll) {
+        int num = ll.get(0);
+        ll.remove(0);
+        return num;
+    }
+
+    public static int first(LinkedList<Integer> ll) {
+        int num = ll.get(0);
+        return num;
+    }
+
     
 }
